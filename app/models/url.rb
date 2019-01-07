e5d99a5246b145f11ca010fc38db99e2034b0edd
Rulon8 <giancalonghi@hotmail.com>
2019-01-07 12:12:25 -0600

    Add json formatter to Url model
    This ensures that json rendering of Url objects will only include relevant
    attributes. This was made to minimize network data usage and as a security
    measure to prevent internal system data to leak outside the system. For
    example, data such as the internal database id of a record or its last
    modified date should not be publicly available.

--- app/models/url.rb
+++ app/models/url.rb
@@ -4,4 +4,17 @@ class Url < ApplicationRecord
   validates :short_url, format: { with: /[a-zA-Z0-9]/, allow_nil: true, message: "invalid short url" }
   validates :original_url, format: { with: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*)/, message: "invalid original url" }
   validates :visit_count, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: "count must be greater than 0" }
+
+  # Formats a Url object as JSON, removing unnecessary attributes such
+  # as id and created_at.
+  #
+  # Examples
+  #
+  #   render json: my_url_object
+  #   # => {"title":"Google","short_url":"b","original_url":"https://www.google.com/","visit_count":0}
+  #
+  # Returns a JSON object with the selected values.
+  def as_json(options={})
+    super(only: [:title,:short_url,:original_url,:visit_count])
+  end
 end
