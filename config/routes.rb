8f10a546c22724da2b9117dc3b9ea226bc019efd
Rulon8 <giancalonghi@hotmail.com>
2019-01-06 18:11:35 -0600

    Allow only json requests when generating new URLs
    Allows only .json requests to the home URL used for generating new
    short URLs. This is important since there could potentially exist
    a short URL named 'url', so it would collide with the home address
    'localhost/url'. With this change requests to 'localhost/url.json'
    can be safely routed to the controller that generates short URLs
    while 'localhost/url' can be routed to the registered URL with the
    code 'url'. This pattern will be used for all future URLs such as
    'localhost/top' and 'localhost/.top.json'

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'url', to: 'urls#new', constraints: lambda { |req| req.format == :json }
+  post 'url', to: 'urls#new', constraints: lambda { |req| req.format == :json }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
