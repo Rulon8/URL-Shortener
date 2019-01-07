017c58f2bcfab36d1b751b291ed94fc01def9e58
Rulon8 <giancalonghi@hotmail.com>
2019-01-06 21:10:33 -0600

    Add support for routes containing shortened URLs
    Routing is configured to accept GET requests for any valid short URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'url', to: 'urls#new', constraints: lambda { |req| req.format == :json }
   post 'url', to: 'urls#new', constraints: lambda { |req| req.format == :json }
+  get ':short_url', to: 'urls#show', short_url: /[a-zA-z0-9]+/
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
