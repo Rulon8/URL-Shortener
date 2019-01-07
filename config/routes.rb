8e0f94c6e93d864d762d602f8a753014dccc4608
Rulon8 <giancalonghi@hotmail.com>
2019-01-07 12:04:58 -0600

    Add top.json route to get the top 100 most used URLs
    Just as url.json it only accepts json format requests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'url', to: 'urls#new', constraints: lambda { |req| req.format == :json }
   post 'url', to: 'urls#new', constraints: lambda { |req| req.format == :json }
+  get 'top', to: 'urls#top', constraints: lambda { |req| req.format == :json }
   get ':short_url', to: 'urls#show', short_url: /[a-zA-z0-9]+/
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
