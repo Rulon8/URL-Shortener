71eb2b3975a2c9ed3e5d05ecb0a37da99f15e97b
Giancarlo <giancalonghi@hotmail.com>
2019-01-12 23:33:32 +0000

    Create basic React app structure
    Includes a Form and a Message component where a success or failure
    message will be shown.

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Form = (props) => {
+  return (
+    <div>
+      <form>
+        <input type="text"/>
+        <button type="submit" className="btn">GO</button>
+      </form>
+    </div>
+  )
+}
+
+const Message = (props) => {
+  return (
+    <div className={props.class}>
+      {props.message}
+    </div>
+  )
+}
+
+class Shortener extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {shortened: "pending"};
+  }
+  
+  render() {
+    return (
+      <div>
+        <Form/>
+        <Message class={this.state.shortened} message="SHOW"/>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <Shortener />
       </div>
     );
   }
