f6d9d56ad09693f0c873d66d170b0ec4faf6df21
Giancarlo <giancalonghi@hotmail.com>
2019-01-13 05:24:28 +0000

    Add the Top section of the App
    Includes a button that if clicked, shows a table with the information
    of the top 100 most visited URLs.

--- client/src/App.js
+++ client/src/App.js
@@ -81,13 +81,95 @@ class Shortener extends Component {
   }
 }
 
+/* When clicked calls function that displays 100 top URLs */
+const TopButton = (props) => {
+  return (
+    <div>
+     <button type='button' onClick={props.onClickFunction}>Top 100 URLs</button> 
+    </div>
+  )
+}
 
+/* Shows a table with the title, original URL, short URL and number if registered
+ * visits of each of the top 100 URLs with most visits. If there are less than 100
+ * URLs registered in the system shows them all, and if there are none shows a 'No
+ * data available' message.
+ */
+const TopTable = (props) => {
+  let content;
+  if (props.status === 'success') {
+    if (props.tableData.length == 0) {
+      content = 'No data available';
+    } else {
+      content = <table>
+         <thead>
+          <tr>
+            <th>Title</th>
+            <th>Long URL</th>
+            <th>Short URL</th>
+            <th>Visit Count</th>
+          </tr>
+         </thead>
+         <tbody>
+           {props.tableData.map(function(url, index) {
+             let href = "https://" + window.location.hostname + ":8081/";
+             return <tr>
+                      <td>{url.title}</td>
+                      <td>{url.original_url}</td>
+                      <td><a href={href + url.short_url}>{url.short_url}</a></td>
+                      <td>{url.visit_count}</td>
+                    </tr>;
+           })}
+         </tbody>
+       </table>;
+    }
+  }
+  return (
+    <div>
+     {content}
+    </div>
+  )
+}
 
+/* Detects if the user has asked for the top 100 URLs. If the button has been
+ * clicked sends a GET request to the API's /top.json and sends the data to
+ * the TopTable to be rendered.
+ */
+class Top extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {status: 'pending', data: []};
+  }
+  
+  buttonClickListener = () => {
+    fetch('/top.json')
+    .then((response) => {
+      return response.json();
+    }).then((jsonResponse) => {
+      console.log(jsonResponse);
+      this.setState({status: 'success', data: jsonResponse})
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <TopButton onClickFunction={this.buttonClickListener}/>
+        <TopTable status={this.state.status} tableData={this.state.data}/>
+      </div>
+    );
+  }
+}
+
+/* Main component of the App, contains the Shortener and Top URLs sections */
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Shortener />
+        <Top />
       </div>
     );
   }
